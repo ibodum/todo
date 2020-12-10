4bd60d6e3c47839bb3341d37785401dfdf6ecc57
ibodum <62959339+ibodum@users.noreply.github.com>
2020-12-10 03:23:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,61 +1,46 @@
 var button = document.getElementById('enter');
 var input = document.getElementById("userinput");
-var ul = document.querySelector("ul");
+var ul = document.querySelector('ul');
 var dltbutton = document.getElementsByClassName("delete")[0];
 var li = document.getElementsByTagName("li");
-
-
-
-function inputLength () {
-	return input.value.length;
-}
-
-
+var h1= document.querySelector("h1");
 
 function createListElement() {
-		var li = document.createElement("li");
-		li.appendChild(document.createTextNode(input.value));
-		ul.appendChild(li);
-		input.value = "";
+		var li = document.createElement("li",);
+	li.appendChild(document.createTextNode(input.value,));
+	ul.appendChild(li);
+	input.value = '';
 
-		var button = document.createElement("button");
+	var button = document.createElement("button");
 		button.appendChild(document.createTextNode("delete"));
 		li.appendChild(button);
 
+
 	    button.onclick=removeParent;
-//THIS FUNCTION WILL DO THIS FOR EVERY BUTTON CREAT
 
-
-	function underlineParent(event){
+}
+function underlineParent(e){
 		event.target.parentNode.classList.toggle("done");
 	}
-
-
-	function removeParent(evt){
-		evt.target.parentNode.remove();
+function removeParent(e){
+		event.target.parentNode.remove();
 	} 
 
+function inputLength() {
+	return input.value.length
 }
 
-
-function addListAterClick() {
-	if (inputLength () > 0) {
-		createListElement();
-
-	}
-
-}
-
-function addListAfterKeyPres(e) {
-	if (inputLength () > 0 && event.keyCode === 13) {
-		createListElement();
-
-
+function addListAfterClick() {
+	if(inputLength()> 0) {
+	  createListElement() 
 	}
 }
+function addListAfterKeypress(e){
+	if(inputLength() > 0 && event.which === 13) {
+	createListElement() 
+    }
+}
 
-
-
-button.addEventListener("click", addListAterClick);
-
-input.addEventListener("keypress",addListAfterKeyPres);
+button.addEventListener("click",addListAfterClick );
+	
+input.addEventListener("keypress", addListAfterKeypress);
