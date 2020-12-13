7c0b6dbe556fe3b78c45995c53f4e33a07d5207e
ibodum <62959339+ibodum@users.noreply.github.com>
2020-12-13 03:01:38 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,49 @@ var ul = document.querySelector('ul');
 var dltbutton = document.getElementsByClassName("delete")[0];
 var li = document.getElementsByTagName("li");
 var h1= document.querySelector("h1");
+var button1 = document.getElementsByClassName("button1")[0];
+var button2 = document.getElementsByClassName("button2")[0];
+var button3 = document.getElementsByClassName("button3")[0];
+var button4 = document.getElementsByClassName("button4")[0];
+var message = document.querySelector("h3");
+
+var mesArray = ["do what ever your wife says, happy wife ,happy life :)", 
+"ohh sh.t you spent all your money!",
+"i wish i would not ever listen that fu.king voice!!!",
+"which idoit sad money does  not bring happyness. idoit!!!",
+"i love to spent moneyyyyyyyyyyyyyy ha ha ha ",
+ "money money where is honey"]
+
+function addeventtoall() {
+	button1.addEventListener("click", function() {
+	    var message1 = mesArray
+	    const randomIndex = Math.round(Math.random()*mesArray.length);
+	    message.textContent = mesArray[randomIndex]
+
+	})
+	button2.addEventListener("click", function() {
+		var message2 = mesArray
+		const randomIndex = Math.round(Math.random()*mesArray.length);
+		message.textContent = mesArray[randomIndex]
+	})
+	button3.addEventListener("click", function() {
+		var message3 = mesArray
+		const randomIndex = Math.round(Math.random()*mesArray.length);
+		message.textContent = mesArray[randomIndex]
+	})
+	button4.addEventListener("click", function() {
+		var message4 = mesArray
+		const randomIndex = Math.round(Math.random()*mesArray.length);
+		message.textContent = mesArray[randomIndex]
+	})
+	
+	}
+
+
+
+addeventtoall();
+
+
 
 function createListElement() {
 		var li = document.createElement("li",);
@@ -40,6 +83,7 @@ function addListAfterKeypress(e){
 	createListElement() 
     }
 }
+button1 = button.addEventListener("click", addListAfterClick);
 
 button.addEventListener("click",addListAfterClick );
 	
